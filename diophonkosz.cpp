6d5a408475fe1563317bdb9784f3fe87e176a4cb
Balazsoficial <193341121+Balazsoficial@users.noreply.github.com>
2025-04-26 21:43:57 +0200

    Fixed up some exceptions

--- diophonkosz.cpp
+++ diophonkosz.cpp
@@ -29,44 +29,45 @@ int main() {
     cout << (e*b*b-c*b*d+a*d*d)<<endl;
     if (d<0){balancer= abs(d);}
     else if (d>0){balancer= -abs(d);}
-
-    for (int i=-abs((e*b*b-c*b*d+a*d*d)+balancer);i<=abs((e*b*b-c*b*d+a*d*d)+balancer);i++) {
-        if (((e*b*b-c*b*d+a*d*d)/(b*i+d))==1||((e*b*b-c*b*d+a*d*d)/(b*i+d)==-1))
+    if(b==0) {
+        cout << "Mivel XY 0 ezért a függvénynek vagy végtelen vagy 0 megoldása van és nem megoldható polinomokkal" <<endl;
+    }
+    else {
+        for (int i=-abs((e*b*b-c*b*d+a*d*d)+balancer);i<=abs((e*b*b-c*b*d+a*d*d)+balancer);i++) {
+            if (((e*b*b-c*b*d+a*d*d)/(b*i+d))==1||((e*b*b-c*b*d+a*d*d)/(b*i+d)==-1))
             {
                 cout << "Szelso ertek x:  " << i << endl;
                 szelsok.push_back(i);
                 //cout << ((e*b*b-c*b*d+a*d*d)/(b*i-d)) << endl;
             }
-        else if(((e*b*b-c*b*d+a*d*d)/(b*i-d))==0) {
-            if (szelsok.empty())
-            {x=0;
-                if (((a*b*x+c*b-a*d + (e*b*b-c*b*d+a*d*d)/b*d*x)/(b*b)) == static_cast<int>(a*b*x+c*b-a*d + (e*b*b-c*b*d+a*d*d)/b*d*x)/(b*b)) {
-                    cout << -(a*b*x+c*b-a*d + (e*b*b-c*b*d+a*d*d)/b*d*x)/(b*b)<<" es barmely szam kombinacioja" <<endl;
-                }
+            else if(((e*b*b-c*b*d+a*d*d)/(b*i-d))==0) {
+                if (szelsok.empty())
+                {x=0;
+                    if (((a*b*x+c*b-a*d + (e*b*b-c*b*d+a*d*d)/b*d*x)/(b*b)) == static_cast<int>(a*b*x+c*b-a*d + (e*b*b-c*b*d+a*d*d)/b*d*x)/(b*b)) {
+                        cout << -(a*b*x+c*b-a*d + (e*b*b-c*b*d+a*d*d)/b*d*x)/(b*b)<<" es barmely szam kombinacioja" <<endl;
+                    }
 
 
-            exit(0);}
+                    exit(0);}
+
+            }
 
         }
 
+        cout << szelsok.back()<<endl;
+        for (x=szelsok[0];x<=szelsok.back();x++) {
+            double eredmeny = ((a*b*x+c*b-a*d) + (e*b*b-c*b*d+a*d*d)/(b*x+d))/(b*b) ;
+            cout << eredmeny<<endl;
+            if (isnan(eredmeny)) {
+                cout << "ez nem megoldható mivel: polinómos módon mivel végtelen megoldás lesz" << (e*b*b-c*b*d+a*d*d) <<"/" << b*x+d << "/" << (b*b)<<endl;
+                cout << "és ez nem egy számot ad!";
+            }
+
+            if (eredmeny == static_cast<int>(eredmeny)){
+                cout << -(a*b*x+c*b-a*d + (e*b*b-c*b*d+a*d*d)/(b*x+d))/(b*b)<<" ------- x: " <<x <<endl;
+            }
+
+
+        }
     }
-
-    cout << szelsok.back()<<endl;
-    for (x=szelsok[0];x<=szelsok.back();x++) {
-        double eredmeny = ((a*b*x+c*b-a*d) + (e*b*b-c*b*d+a*d*d)/(b*x+d))/(b*b) ;
-        cout << eredmeny<<endl;
-        if (isnan(eredmeny)) {
-            cout << "ez nem megoldható mivel: polinómos módon mivel végtelen megoldás lesz" << (e*b*b-c*b*d+a*d*d) <<"/" << b*x+d << "/" << (b*b)<<endl;
-            cout << "és ez nem egy számot ad!";
-        }
-
-        if (eredmeny == static_cast<int>(eredmeny)){
-            cout << -(a*b*x+c*b-a*d + (e*b*b-c*b*d+a*d*d)/(b*x+d))/(b*b)<<" ------- x: " <<x <<endl;
-        }
-
-
-    }
-
-
-
 }
