21cdee4eda732c5ab34efdc21a5c8bc3c4efff03
Balazsoficial <193341121+Balazsoficial@users.noreply.github.com>
2025-09-05 16:23:10 +0200

    297

---
+++ 279.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Balazsoficial on 2025. 09. 05..
+//
+#include <iostream>
+using namespace std;
+int main() {
+    for (long long i =5; i<=9223372036854775807;i=i+10){
+       long long   solution = i * 297;
+       // cout << "a" << endl;
+        if (((solution - 1995)%10000 )==0 && (solution - 1995)>10000) {
+            cout <<"Megoldas: " << solution  ;
+            cout << "   eredeti: " << i << endl;
+        }
+        //Megoldas: 841995   eredeti: 2835
+       // Megoldas: 3811995   eredeti: 12835
+       // Megoldas: 6781995   eredeti: 22835
+
+
+
+    }
+
+}
